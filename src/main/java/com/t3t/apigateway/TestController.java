556b59a9042120005e497057232bdb713db1009c
woody35545 <woody35545@gmail.com>
2024-03-14 19:36:02 +0900

    init: api-gateway 프로젝트

---
+++ src/main/java/com/t3t/apigateway/TestController.java
@@ -0,0 +1,12 @@
+package com.t3t.apigateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test() {
+        return "v0.0.1";
+    }
+}
