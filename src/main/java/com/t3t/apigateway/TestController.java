a1f31c97dc3cab5ae9431286e309702137279636
Woody_K <woody35545@gmail.com>
2024-03-14 20:06:39 +0900

    test: #1 cicd workflow 테스트

--- src/main/java/com/t3t/apigateway/TestController.java
+++ src/main/java/com/t3t/apigateway/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String test() {
-        return "v0.0.2";
+        return "v0.0.3";
     }
 }
