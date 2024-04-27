0afbc5d251c6aee24f21ddccff03df62473986ec
woody35545 <woody35545@gmail.com>
2024-04-28 07:27:18 +0900

    feat: #10 `ApiGatewayApplication` - ConfigurationPropertiesScan 추가

--- src/main/java/com/t3t/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/t3t/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,9 @@ package com.t3t.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@ConfigurationPropertiesScan
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
