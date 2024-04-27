271687d67a48ae05d45b4e650d062f522ec4d881
woody35545 <woody35545@gmail.com>
2024-04-28 07:30:26 +0900

    feat: #10 `SecretKeyManagerResponse ` - Secret Key Manager API의 응답 형식을 정의한 클래스

---
+++ src/main/java/com/t3t/apigateway/keymanager/properties/model/response/SecretKeyManagerResponse.java
@@ -0,0 +1,26 @@
+package com.t3t.apigateway.keymanager.properties.model.response;
+
+import lombok.Getter;
+
+/**
+ * Secret Key Manager API의 응답 형식을 정의한 클래스
+ *
+ * @author woody35545(구건모)
+ */
+@Getter
+public class SecretKeyManagerResponse {
+    private SecretKeyManagerResponseHeaderPartDto header;
+    private SecretKeyManagerResponseBodyPartDto body;
+
+    @Getter
+    public static class SecretKeyManagerResponseHeaderPartDto {
+        private int resultCode;
+        private String resultMessage;
+        private String isSuccessful;
+    }
+
+    @Getter
+    public static class SecretKeyManagerResponseBodyPartDto {
+        private String secret;
+    }
+}
